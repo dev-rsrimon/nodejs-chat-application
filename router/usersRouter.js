4bf7a49e12974216d5b8514e4037951460f6c650
Rs. Rimon <rimon@workspacify.com>
2022-03-27 01:54:22 +0600

    user add done

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -2,12 +2,26 @@
 const express = require("express");
 
 // internal imports
-const { getUsers } = require("../controller/usersController");
+const { getUsers, addUser } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avaterUpload = require("../middlewares/users/avaterUpload");
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/userValidator");
 
 const router = express.Router();
 
 // users page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+// add user
+router.post(
+  "/",
+  avaterUpload,
+  addUserValidators,
+  addUserValidationHandler,
+  addUser
+);
 
 module.exports = router;
